66938ec97db1179b59073dcc84fad41fff05944a
ilcommitfa <110323544+ilcommitfa@users.noreply.github.com>
2022-12-16 18:02:52 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -7,19 +7,19 @@ const gallery = document.querySelector('.gallery');
 const items = [];
 
 galleryItems.forEach(element => {
-	const galleryLink = document.createElement('a')
-	galleryLink.className = 'gallery__link'
-	galleryLink.href = element.original
-	const galleryImage = document.createElement('img')
-	galleryImage.className = 'gallery__image'
-	galleryImage.src = element.preview
-	galleryImage.setAttribute('title', element.description)
-	galleryImage.alt = element.description
+	const galleryLink = document.createElement('a');
+	galleryLink.className = 'gallery__link';
+	galleryLink.href = element.original;
+	const galleryImage = document.createElement('img');
+	galleryImage.className = 'gallery__image';
+	galleryImage.src = element.preview;
+	galleryImage.setAttribute('title', element.description);
+	galleryImage.alt = element.description;
 
-	galleryLink.append(galleryImage)
-	items.push(galleryLink)
-})
-gallery.append(...items)
+	galleryLink.append(galleryImage);
+	items.push(galleryLink);
+});
+gallery.append(...items);
 
 new SimpleLightbox('.gallery a', {
 	captionDelay: 250
